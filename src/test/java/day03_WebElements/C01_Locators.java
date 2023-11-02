6847ecba34ebf06125ec990298a93c47a0f4c01c
Serhan Subaşı <subasiserhan@gmail.com>
2023-11-02 12:39:43 +0300

    Selenium dersleri day 03 part 1

---
+++ src/test/java/day03_WebElements/C01_Locators.java
@@ -0,0 +1,52 @@
+package day03_WebElements;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class C01_Locators {
+    public static void main(String[] args) {
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+
+/*
+        //Amazon sayfasına gidiniz
+        driver.get("https://amazon.com");
+
+        //Arama kutusunu locate ediniz ve iphone yazdırıp aratınız
+        WebElement aramaKutusu = driver.findElement(By.id("twotabsearchtextbox"));
+        aramaKutusu.sendKeys("iphone"); //yazı yazdırır
+        aramaKutusu.submit();   //enter tuşuna basar
+
+        //sayfayı kapatınız
+        driver.close();
+*/
+
+        // 2. YOL -WebElement kutusuna koymadan
+        //Amazon sayfasına gidiniz
+        driver.get("https://amazon.com");
+
+        //Arama kutusunu locate ediniz ve iphone yazdırıp aratınız
+        driver.findElement(By.id("twotabsearchtextbox")).sendKeys("iphone", Keys.ENTER);
+
+        //sayfayı kapatınız
+        driver.close();
+
+/*
+Locate ettiğimiz WebElement <input> tagına sahipse, o WebElement'e sendKeys mehtodu ile istediğimiz texti gönderebiliriz.
+Eğer bir arama kutusunu locate edip text gönderirsek bunu iki şekilde enter yaptırabiliriz.
+1) submit() methodu
+2) Keys.ENTER
+ */
+
+
+    }
+}
