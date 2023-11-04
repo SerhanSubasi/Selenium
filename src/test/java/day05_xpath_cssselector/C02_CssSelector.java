811bc84350d11d591f1ad1d7321e8acba06439d3
Serhan Subaşı <subasiserhan@gmail.com>
2023-11-05 00:41:47 +0300

    Selenium dersleri day 05

---
+++ src/test/java/day05_xpath_cssselector/C02_CssSelector.java
@@ -0,0 +1,71 @@
+package day05_xpath_cssselector;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class C02_CssSelector {
+    public static void main(String[] args) throws InterruptedException {
+
+/*
+SYNTAX
+
+1) tagName[attributeName='attributeValue']
+2) eger id ile css locate almak istersek #idAttributeDegeri
+3) eger class ile css locate almak istersen .classAttributeDegeri
+
+XPATH ile CSS SELECTOR arasındaki farklar
+
+1) cssSelector xpath'e göre daha hızlı çalışır.
+2) Xpath ile bir WebElement için birden fazla sonuc verirse index ile bunu teke düşürebiliriz
+        fakat css selector ile index rahatça kullanamayız.
+3) Xpath ile bir tag'a sahip webElement'in sadece texti ile locate edebiliriz.
+        Fakat css ile bunu yapamayız.
+
+ */
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+
+
+        //https://the-internet.herokuapp.com/add_remove_elements/ adresine gidin
+        //Add Element butonuna basin
+        //Delete butonu'nun gorunur oldugunu test edin
+        //Delete tusuna basin
+        //"Add/Remove Elements" yazisinin gorunur oldugunu test edin
+        //sayfayı kapatınız
+
+        //https://the-internet.herokuapp.com/add_remove_elements/ adresine gidin
+        driver.get("https://the-internet.herokuapp.com/add_remove_elements/");
+
+        //Add Element butonuna basin
+        driver.findElement(By.cssSelector("button[onclick ='addElement()' ]")).click();
+        Thread.sleep(3000);
+
+        //Delete butonu'nun gorunur oldugunu test edin
+        WebElement deleteButton = driver.findElement(By.cssSelector("button[class='added-manually']"));
+        if(deleteButton.isDisplayed()){
+            System.out.println("TEST PASSED");
+        }else {
+            System.out.println("TEST FAILED");
+        }
+
+        //Delete tusuna basin
+        Thread.sleep(3000);
+        deleteButton.click();
+
+        //"Add/Remove Elements" yazisinin gorunur oldugunu test edin
+        Thread.sleep(2000);
+        WebElement addRemoveText = driver.findElement(By.cssSelector("h3"));
+        System.out.println(addRemoveText.isDisplayed() ? "TEST PASSED" : "TEST FAILED");
+
+        //sayfayı kapatınız
+        driver.close();
+
+    }
+}
---
+++ src/test/java/day05_xpath_cssselector/C04_RelativeLocators.java
@@ -0,0 +1,39 @@
+package day05_xpath_cssselector;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import static org.openqa.selenium.support.locators.RelativeLocator.with;
+import java.time.Duration;
+
+public class C04_RelativeLocators {
+    public static void main(String[] args) {
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+
+        //https://www.diemol.com/selenium-4-demo/relative-locators-demo.html adresine gidin
+        driver.get("https://www.diemol.com/selenium-4-demo/relative-locators-demo.html");
+
+        //Berlin'i relative locator ile locate edin
+        WebElement NYCWebElement = driver.findElement(By.id("pid3_thumb"));
+        WebElement BayAreaWebElement = driver.findElement(By.id("pid8_thumb"));
+        WebElement berlinWebelement = driver.findElement(with(By.tagName("img")).below(NYCWebElement).toLeftOf(BayAreaWebElement));
+        berlinWebelement.click();
+
+        //Relative locator'larin dogru calistigini test edin
+
+        String actualIdDegeri = berlinWebelement.getAttribute("id");
+        String expectedIdDegeri = "pid7_thumb";
+
+        System.out.println(actualIdDegeri.equals(expectedIdDegeri) ? "TEST PASSED" : "TEST FAILED");
+
+        //sayfayı kapatınız
+        driver.close();
+
+    }
+}
